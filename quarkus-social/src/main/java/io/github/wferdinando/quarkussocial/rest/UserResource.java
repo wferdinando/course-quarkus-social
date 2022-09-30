6a4cec7f4de3bf6a11b4aecbe9d771a4855ed68f
WillyanFerdinando <willyantferdinando@gmail.com>
2022-09-30 15:42:19 -0300

    BASIC CRUD USERS

--- quarkus-social/src/main/java/io/github/wferdinando/quarkussocial/rest/UserResource.java
+++ quarkus-social/src/main/java/io/github/wferdinando/quarkussocial/rest/UserResource.java
@@ -17,7 +17,7 @@ public class UserResource {
 
     @POST
     @Transactional
-    public Response createUser( CreateUserRequest userRequest){
+    public Response createUser(CreateUserRequest userRequest) {
         User user = new User();
         user.setName(userRequest.getName());
         user.setAge(userRequest.getAge());
@@ -28,11 +28,36 @@ public class UserResource {
     }
 
     @GET
-    public Response listAllUsers(){
+    public Response listAllUsers() {
         PanacheQuery<PanacheEntityBase> query = User.findAll();
         return Response.ok(query.list()).build();
     }
 
-    
+    @PUT
+    @Path("{id}")
+    @Transactional
+    public Response updateUser(@PathParam("id") Long id, CreateUserRequest userData) {
+        User user = User.findById(id);
+
+        if (user != null) {
+            user.setName(userData.getName());
+            user.setAge(userData.getAge());
+            return Response.ok(user).build();
+        }
+
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Transactional
+    public Response deleteUser(@PathParam("id") Long id) {
+        User user = User.findById(id);
+        if (user != null) {
+            user.delete();
+            return Response.ok().build();
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
 
 }
