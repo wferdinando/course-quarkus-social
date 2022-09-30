c7858c9e974b8b699f3db47985e3749c83b0363a
WillyanFerdinando <willyantferdinando@gmail.com>
2022-09-30 11:51:16 -0300

    Entity User,POST, GET OK

--- quarkus-social/src/main/java/io/github/wferdinando/quarkussocial/rest/UserResource.java
+++ quarkus-social/src/main/java/io/github/wferdinando/quarkussocial/rest/UserResource.java
@@ -1,17 +1,38 @@
 package io.github.wferdinando.quarkussocial.rest;
 
+import io.github.wferdinando.quarkussocial.model.User;
 import io.github.wferdinando.quarkussocial.rest.dto.CreateUserRequest;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
 
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
 
     @POST
+    @Transactional
     public Response createUser( CreateUserRequest userRequest){
-        return Response.ok().build();
+        User user = new User();
+        user.setName(userRequest.getName());
+        user.setAge(userRequest.getAge());
+
+        user.persist();
+
+        return Response.ok(user).build();
     }
+
+    @GET
+    public Response listAllUsers(){
+        PanacheQuery<PanacheEntityBase> query = User.findAll();
+        return Response.ok(query.list()).build();
+    }
+
+    
 
 }
