41ab8cd84fc51984be72229367c8443af346baa9
WillyanFerdinando <willyantferdinando@gmail.com>
2022-10-04 10:40:06 -0300

    Add Lombok

--- quarkus-social/src/main/java/io/github/wferdinando/quarkussocial/domain/rest/dto/CreateUserRequest.java
+++ quarkus-social/src/main/java/io/github/wferdinando/quarkussocial/domain/rest/dto/CreateUserRequest.java
@@ -1,28 +1,15 @@
 package io.github.wferdinando.quarkussocial.domain.rest.dto;
 
+import lombok.*;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+@Data
 public class CreateUserRequest {
 
     @NotBlank(message = "Name is Required!") // verifica se é nula ou vazia
     private String name;
     @NotNull(message = "Age is Required!")
     private Integer age;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
 }
