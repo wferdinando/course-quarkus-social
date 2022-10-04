4c17c17a01cb9cbb50b838cc3bec96a58cc37247
WillyanFerdinando <willyantferdinando@gmail.com>
2022-10-04 11:02:33 -0300

    Create PostResource

---
+++ quarkus-social/src/main/java/io/github/wferdinando/quarkussocial/domain/rest/PostResource.java
@@ -0,0 +1,24 @@
+package io.github.wferdinando.quarkussocial.domain.rest;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/users/{userId}/posts")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class PostResource {
+
+    @POST
+    public Response savePost(){
+
+        return Response.status(Response.Status.CREATED).build();
+    }
+
+    @GET
+    public Response listPosts(){
+
+        return Response.ok().build();
+    }
+
+}
